08bc389a1b3a6c685ab89967b75be59858264b92
ochukoemmanuel <ochukoemmanuel07@gmail.com>
2021-07-12 00:14:19 +0100

    Second commit

---
+++ app/src/main/java/com/ebeatsz/rectempastqa/CourseScreen.java
@@ -0,0 +1,14 @@
+package com.ebeatsz.rectempastqa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CourseScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_course_screen);
+    }
+}
---
+++ app/src/main/java/com/ebeatsz/rectempastqa/MainActivity.java
@@ -0,0 +1,20 @@
+
+package com.ebeatsz.rectempastqa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        setContentView(R.layout.activity_main);
+    }
+}
